7543793c780bbbed215d5ea913482c2b57b6c94d
loaiyehia <156128479+loaiyehia@users.noreply.github.com>
2024-01-16 01:38:43 +0200

    react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Html from "./pages/Html"
+import Css from "./pages/Css"
+import JavaScript from "./pages/JavaScript"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement:<h1 style={{color:"red"}}>Sorry!!!!!</h1>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+  
+  },
+  {
+    path: "/javascript",
+    element: <JavaScript/>,
+
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router} />
   </React.StrictMode>
 );
 
