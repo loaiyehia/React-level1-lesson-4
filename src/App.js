3d8a15a72c56a6014eb4cabd2afc4449d020a023
loaiyehia <156128479+loaiyehia@users.noreply.github.com>
2024-01-16 04:21:58 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Css from "./pages/Css";
+import Html from "./pages/Html";
+import JavaScript from "./pages/JavaScript";
 
 function App() {
-return(<div>
-  App.js file
-</div>)
+  return (
+    <div className="App">
+      <h1 style={{ color: "red" }}>Welcome to react router</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/css" element={<Css />}></Route>
+        <Route path="/html" element={<Html />}></Route>
+        <Route path="/javascript" element={<JavaScript />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-import Home from './pages/Home';
-import Html from "./pages/Html"
-import Css from "./pages/Css"
-import JavaScript from "./pages/JavaScript"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-    errorElement:<h1 style={{color:"red"}}>Sorry!!!!!</h1>
-  },
-  {
-    path: "/html",
-    element: <Html/>,
-  },
-  {
-    path: "/css",
-    element: <Css/>,
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home/>,
+//     errorElement:<h1 style={{color:"red"}}>Sorry!!!!!</h1>
+//   },
+//   {
+//     path: "/html",
+//     element: <Html/>,
+//   },
+//   {
+//     path: "/css",
+//     element: <Css/>,
   
-  },
-  {
-    path: "/javascript",
-    element: <JavaScript/>,
+//   },
+//   {
+//     path: "/javascript",
+//     element: <JavaScript/>,
 
-  },
-]);
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<RouterProvider router={router} />
+    <BrowserRouter>
+<App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
